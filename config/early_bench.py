d46a8300318096fd9fe026aef57955d2ee5dff16
slamduncaan747 <43890142+slamduncaan747@users.noreply.github.com>
2025-07-02 19:07:56 -0400

    better logging

--- config/early_bench.py
+++ config/early_bench.py
@@ -2,12 +2,13 @@
 # This should give you much better performance
 
 out_dir = "out-shakespeare"
-eval_interval = 500  # less frequent evaluation
+eval_interval = 50  # less frequent evaluation
 eval_iters = 50  # fewer eval iterations
+save_interval = 2000
 log_interval = 10  # more frequent logging to see progress
 
 always_save_checkpoint = False
-wandb_log = False
+wandb_log = True
 wandb_project = "shakespeare"
 wandb_run_name = "mini-gpt"
 
@@ -19,14 +20,14 @@ batch_size = 16  # Was 16, now 64 - better GPU utilization
 block_size = 512  # Was 1024, now 512 - fits in memory better
 
 # Model size (this is fine)
-n_layer = 6
-n_head = 6
+n_layer = 12
+n_head = 12
 n_embd = 768
 dropout = 0.2
 
-learning_rate = 5e-3
-max_iters = 500  # Reduced for testing
-lr_decay_iters = 500
+learning_rate = 1e-3
+max_iters = 2000  # Reduced for testing
+lr_decay_iters = 2000
 min_lr = 1e-4
 beta2 = 0.99
 warmup_iters = 100
